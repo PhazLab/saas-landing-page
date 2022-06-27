08000d0ad1a2944af71cd6df568fb882193a9509
Samuel Bouye <bouyesamuel@gmail.com>
2022-06-27 23:16:07 +0000

    add content value

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -4,9 +4,9 @@ import ContentItem from './ContentItem'
 const Content = () => {
   return (
     <ul className='content_items'>
-        <ContentItem title="10%" content="Test add content" />
-        <ContentItem title="10%" content="Test add content" />
-        <ContentItem title="10%" content="Test add content" />
+        <ContentItem title="10+" content="Years Of Experience" />
+        <ContentItem title="1000" content="Apartments" />
+        <ContentItem title="20" content="Awards Gained" />
     </ul>
   )
 }
